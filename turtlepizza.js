d4c9c3a54364bbe5f7d4d366ec2ec97599ebdb28
Chmouel Boudjnah <chmouel@chmouel.com>
2016-12-10 09:34:25 +0100

    move js stuff to its own

---
+++ turtlepizza.js
@@ -0,0 +1,20 @@
+var app=angular.module('Turtle',[]).controller('MyController',function($scope,$interval, $http){
+    var c=0;
+    $scope.image_url = 'img/pizza-animated.gif';
+    $scope.refreshGif = (function(){
+        q = "turtle+ninja+pizza"; // search query
+        request = new XMLHttpRequest;
+        result = $http.get('https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q);
+        result.success(function(data, status, headers, config) {
+            $scope.image_url = data.data.image_url.replace("http:", "https:");
+        })
+    })
+    $interval(function(){ $scope.refreshGif(); }, 10000);
+    //This will hide the DIV by default.
+    $scope.IsVisible = false;
+    $scope.ShowHide = function () {
+        //If DIV is visible it will be hidden and vice versa.
+        $scope.IsVisible = $scope.IsVisible ? false : true;
+    }
+
+});

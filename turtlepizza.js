549e428ac6c55ed8f6d956a7e6af95c09d51225a
Chmouel Boudjnah <chmouel@chmouel.com>
2016-12-28 09:06:03 +0100

    Move

--- turtlepizza.js
+++ turtlepizza.js
@@ -1,5 +1,6 @@
 var app=angular.module('Turtle',[]).controller('MyController',function($scope,$interval, $http){
     var c=0;
+
     $scope.refreshGif = (function(){
         q = "turtle+ninja+pizza"; // search query
         request = new XMLHttpRequest;
@@ -9,7 +10,19 @@ var app=angular.module('Turtle',[]).controller('MyController',function($scope,$i
         })
     })
     $interval(function(){ $scope.refreshGif(); }, 10000);
-    //This will hide the DIV by default.
+
+    $scope.refreshInMobile = (function() {
+        var items = ['img/inmobile/mangepizza-1.jpg',
+                     'img/inmobile/cousine-benj.jpg',
+                     'img/inmobile/deux-ninjas.jpg',
+                     'img/inmobile/mangepizza-4.jpg', ]
+        $scope.imgInMobile = items[Math.floor(Math.random()*items.length)];
+    })
+    //default
+    $scope.imgInMobile = 'img/inmobile/silou-pizza.jpg'
+    $interval(function(){ $scope.refreshInMobile(); }, 10000);
+
+    //This will hide the DIV by default. TOREMOVE
     $scope.IsVisible = false;
     $scope.ShowHide = function () {
         //If DIV is visible it will be hidden and vice versa.
